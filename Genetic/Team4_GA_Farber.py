ce5b739d3701cf2e8f9f490c78dcec945d0c9e4c
Tami Farber <38335128+farberta2022@users.noreply.github.com>
2021-12-02 13:05:46 -0500

    method to crossover and mutate for children complete need to work out a few issues random generation complete fitness measure complete list of dictionaries to sort by fitness complete partially complete driver mostly complete reproduction

--- Genetic/Team4_GA_Farber.py
+++ Genetic/Team4_GA_Farber.py
@@ -6,6 +6,7 @@ from operator import itemgetter
 GENERATIONS = 100
 
 GOAL = "Machine Learning is fun!"
+STRAND_LEN = len(GOAL)
 
 CHROMOSOMES = []
 MATCH = False
@@ -22,9 +23,7 @@ def get_nucleotides():
 
 def genetic_code():
     # create a string of random bases the same length as the GOAL code
-    strand_len = len(GOAL)
-    # print(f"strand_len = {strand_len}")
-    chromosome = "".join(get_nucleotides() for _ in range(strand_len))
+    chromosome = "".join(get_nucleotides() for _ in range(STRAND_LEN))
     return chromosome
 
 
@@ -37,11 +36,22 @@ def fitness_test(chromosome):
     fitness = len(GOAL) - difference
     return fitness
 
+def get_parent():
+    parent = {}
+    for _ in range(GENERATIONS):
+        chromosome = genetic_code()
+        fitness = fitness_test(chromosome)
+        parent['chromosome'] = chromosome
+        parent['fitness'] = fitness
+        CHROMOSOMES.append(copy.deepcopy(parent))
+    return CHROMOSOMES
+
 
 def reproduction(gene_pool):
     rank = 0
     next_generation = []
     splitPairs = []
+    TEMP = []
 
     for genes in gene_pool:
         fitness_probability = genes["fitness"] / (len(GOAL))
@@ -56,33 +66,61 @@ def reproduction(gene_pool):
             splitPairs.append(genes)
 
 
-    for _ in range (len(splitPairs)):
+
+    def recombination(gamete1, gamete2):
+        # num of loop iterations is controlled by length of chromosome in gene pool *** should stay in GOAL length
+        crossORmutate = random.uniform(0, 1)
+        offspring = []
+
+        for gene1, gene2 in zip(gamete1["chromosome"], gamete2["chromosome"]):
+            # print(f"{gene1}\t{gene2}")
+            if crossORmutate < .45:
+                offspring.append(gene1)
+            elif crossORmutate < .9:
+                offspring.append(gene2)
+            else:
+                offspring.append(get_nucleotides())
+
+        f1_chromosome = "".join(genes for genes in offspring)
+
+        return f1_chromosome
+
+    def get_nextGen(gamete1, gamete2):
+        parent = {}
+        for _ in range(off_size):
+            chromosome = recombination(gamete1, gamete2)
+            fitness = fitness_test(chromosome)
+            parent['chromosome'] = chromosome
+            parent['fitness'] = fitness
+            TEMP.append(copy.deepcopy(parent))
+        return TEMP
+
+    off_size = len(splitPairs)
+    for _ in range(off_size):
         gamete1 = random.choice(splitPairs[:50])
         gamete2 = random.choice(splitPairs[:50])
-        diploid = recombination(gamete1, gamete2)
+        get_nextGen(gamete1, gamete2)
 
-
-def recombination(gamete1, gamete2):
-    #TODO perform crossover and mutation based on random
-    pass
-
-
+    #TODO clear global CHROMOSOME to replace with next_generation and recombination chromosomes
+    #TODO append the two lists of dicts into the empty CHROMOSOME
 
 
 if __name__ == '__main__':
     epoch = 1
     print(f"{GOAL:>42}")
-    parent = {}
-    for _ in range(GENERATIONS):
-        chromosome = genetic_code()
-        fitness = fitness_test(chromosome)
-        parent['chromosome'] = chromosome
-        parent['fitness'] = fitness
-        CHROMOSOMES.append(copy.deepcopy(parent))
+    # TODO sort out flow of driver and document methods
+    get_parent()
+    # parent = {}
+    # for _ in range(GENERATIONS):
+    #     chromosome = genetic_code()
+    #     fitness = fitness_test(chromosome)
+    #     parent['chromosome'] = chromosome
+    #     parent['fitness'] = fitness
+    #     CHROMOSOMES.append(copy.deepcopy(parent))
 
-    while not MATCH:
-        gene_pool = sorted(CHROMOSOMES, key=itemgetter('fitness'))
-        reproduction(gene_pool)
+    gene_pool = sorted(CHROMOSOMES, key=itemgetter('fitness'))
+    reproduction(gene_pool)
 
-
-
+    # while not MATCH:
+    #     gene_pool = sorted(CHROMOSOMES, key=itemgetter('fitness'))
+    #     reproduction(gene_pool)
